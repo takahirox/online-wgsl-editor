f66bb534f6008ce0c58a3984aef9dd33f8d6a452
Takahiro <hogehoge@gachapin.jp>
2022-01-18 20:31:39 -0800

    Set GPURenderPassColorAttachment.storeOp

--- src/renderer.js
+++ src/renderer.js
@@ -48,6 +48,7 @@ export default class WGPURenderer {
     this._renderPassDescriptor = {
       colorAttachments: [{
         loadValue: {r: 0.0, g: 0.0, b: 0.0, a: 1.0},
+        storeOp: 'store',
         view: null // setup in render()
       }],
       depthStencilAttachment: {
